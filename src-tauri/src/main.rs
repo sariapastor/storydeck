fb55f1a51bc509fa237626af0b2a727b8c590fa3
Saria Pastor <saria.pastor@protonmail.com>
2022-08-05 21:47:26 -0400

    native window controls on css titlebar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,44 @@ use tokio;
 use mongodb::bson::{Document, doc, oid::ObjectId};
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
+// code to get native macos window controls without titlebar (from here: https://github.com/tauri-apps/tauri/issues/2663)
+use cocoa::appkit::{NSWindow, NSWindowStyleMask};
+use tauri::{Runtime, Window};
+
+pub trait WindowExt {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool) {
+    use cocoa::appkit::NSWindowTitleVisibility;
+
+    unsafe {
+      let id = self.ns_window().unwrap() as cocoa::base::id;
+
+      let mut style_mask = id.styleMask();
+      style_mask.set(
+        NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+        transparent,
+      );
+      id.setStyleMask_(style_mask);
+
+      id.setTitleVisibility_(if transparent {
+        NSWindowTitleVisibility::NSWindowTitleHidden
+      } else {
+        NSWindowTitleVisibility::NSWindowTitleVisible
+      });
+      id.setTitlebarAppearsTransparent_(if transparent {
+        cocoa::base::YES
+      } else {
+        cocoa::base::NO
+      });
+    }
+  }
+}
+
 pub mod db;
 // use db::models::*;
 use db::docs::*;
@@ -87,7 +125,14 @@ async fn main() {
     default_deck: ObjectId::parse_str("62e9a151232b6ea9aab10fa4").expect("failed to parse oid"),
   };
 
+  use tauri::Manager;
+
   tauri::Builder::default()
+    .setup(|app| {
+      let win = app.get_window("main").unwrap();
+      win.set_transparent_titlebar(true);
+      Ok(())
+    })
     .menu(menu)
     .manage(state)
     .invoke_handler(tauri::generate_handler![

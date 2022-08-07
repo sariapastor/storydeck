9307e97ae3555ab2be8b6bcb4eecf4441fee3510
Saria Pastor <saria.pastor@protonmail.com>
2022-08-06 21:44:04 -0400

    minor title bar adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,26 +5,12 @@ const Header = ({ title, setUpdating }) => {
   return (
     <header>
       <div data-tauri-drag-region className="titlebar">
-        {/* <div className="titlebar-button" id="titlebar-minimize">
-          <img
-            src="https://api.iconify.design/mdi:window-minimize.svg"
-            alt="minimize"
-          />
-        </div>
-        <div className="titlebar-button" id="titlebar-maximize">
-          <img
-            src="https://api.iconify.design/mdi:window-maximize.svg"
-            alt="maximize"
-          />
-        </div>
-        <div className="titlebar-button" id="titlebar-close">
-          <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
-        </div> */}
         <div className="window-title">
           <h3>{title}</h3>
         </div>
         <Controls setUpdating={setUpdating} />
       </div>
+      <div className="titlebar-spacer"></div>
     </header>
   );
 };

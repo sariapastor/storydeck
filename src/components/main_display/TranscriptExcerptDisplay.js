ea0561e289888a9439f0862958f002323de74cee
Saria Pastor <saria.pastor@protonmail.com>
2022-08-11 15:27:42 -0400

    updates card view to display first line of transcript once transcript is ready

--- src/components/main_display/TranscriptExcerptDisplay.js
+++ src/components/main_display/TranscriptExcerptDisplay.js
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const TranscriptExcerptDisplay = ({ transcript, mediaTime }) => {
   // TODO: convert mediaTime (seconds float) to display format
   // TODO: create 'excerpt' state variable to reset when mediaTime updates
+  const [excerpt, setExcerpt] = useState("");
+  const displayTime = `${Math.floor(mediaTime / 60)}:${
+    mediaTime - Math.floor(mediaTime / 60)
+  }`;
+  useEffect(() => {
+    setExcerpt(transcript.lines ? transcript.lines[0].line : "");
+  }, [transcript]);
   return (
-    <section className={`excerpt-display${transcript.length ? "" : " hidden"}`}>
-      <h2>00:00.00</h2>
-      <h4>[Transcribed text here]</h4>
+    <section className={`excerpt-display ${transcript.lines ? "" : " hidden"}`}>
+      <h2>{displayTime}</h2>
+      <h4>{excerpt}</h4>
     </section>
   );
 };
@@ -17,14 +25,15 @@ TranscriptExcerptDisplay.propTypes = {
       $oid: PropTypes.string,
     }),
     language: PropTypes.string,
-    text: PropTypes.arrayOf(
+    text: PropTypes.string,
+    lines: PropTypes.arrayOf(
       PropTypes.shape({
         startTime: PropTypes.number,
         endTime: PropTypes.number,
         line: PropTypes.string,
       })
     ),
-  }).isRequired,
+  }),
 };
 
 export default TranscriptExcerptDisplay;

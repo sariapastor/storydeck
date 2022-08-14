ef51f46518f296b12561126fbb38a461ef310d88
Saria Pastor <saria.pastor@protonmail.com>
2022-08-14 13:56:36 -0400

    restyles card decks into tape racks

--- src/components/main_display/StoryDeck.js
+++ src/components/main_display/StoryDeck.js
@@ -1,16 +1,30 @@
 import PropTypes from "prop-types";
+import "./StoryDeck.css";
 
 const StoryDeck = ({ deck, updateActive }) => {
   const summary = deck.description ? deck.description : "";
   const setToActive = () => updateActive("deck", deck._id);
   return (
     <div className="deck-container" onClick={setToActive}>
-      <div className="card-element top">
-        <div className="card-display">
-          <h3>{deck.name}</h3>
-          <h4>{summary}</h4>
-        </div>
+      <div className="collection-title">
+        <h3>{deck.name}</h3>
       </div>
+      <div className="cassette-rack">
+        <div className="rack-background"></div>
+        {[0, 1, 2, 3, 4].map((i) => (
+          <div className="tape-slot" key={i}>
+            {i + 1 < deck.cards.length ? (
+              <>
+                <div className="tape-label">{deck.cards[i].name}</div>
+                <div className="tape-case"></div>
+              </>
+            ) : (
+              <div className="empty-slot">+</div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="collection-description">{summary}</div>
     </div>
   );
 };
